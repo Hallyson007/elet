f31f6c36758ceb9e3c050cdd9aebc3672a322875
Feliipe-Silva <phellipedantask800@gmail.com>
2024-03-01 20:53:13 -0300

    Projeto Hallysom

--- index.php
+++ index.php
@@ -10,37 +10,49 @@
 </head>
 
 <body>
+    
     <header>
+        <div>
+            <button onclick="myFunction()">Dark Mode</button>
+        </div>
         <h1>Loja de Eletrônicos</h1>
         <nav>
-            <a href="#">Início</a> |
-            <a href="#">Produtos</a> |
+            <a href="#">Início</a>
+            <a href="#">Produtos</a>
             <a href="#">Contato</a>
         </nav>
     </header>
     <main>
+        
         <!-- Conteúdo principal aqui -->
-        <section>
+        <section id="idSection">
             <h2>Produtos em Destaque</h2>
             <!-- Exemplo de lista de produtos -->
             <ul>
-                <li>Smartphone</li>
-                <li>TV 4K</li>
-                <li>Notebook</li>
+                <li>Fones de ouvidos</li>
+                <li>Luminárias</li>
+                <li>Capas p/ Diversos</li>
             </ul>
 
             <h3>Categorias</h3>
             <ul>
                 <li><a href="#">Celulares</a></li>
-                <li><a href="#">TVs</a></li>
-                <li><a href="#">Computadores</a></li>
+                <li><a href="#">Carregadores</a></li>
+                <li><a href="#">Games Retro 4k</a></li>
+                <li><a href="#">Caixas de som </a></li>
+                <li><a href="#">Fones de ouvidos</a></li>
+                <li><a href="#">Capas p/ Diversos</a></li>
+                <li><a href="#">Luminárias</a></li>
+                <li><a href="#">Mouse e Teclados</a></li>
             </ul>
         </section>
         <img src="img/1.jfif" alt="img">
     </main>
+
     <footer>
-        
         <p>© 2024 Loja de Eletrônicos</p>
+        <p>CIO hacker do bem.</p>
+        <p><small>Hallyson Developer</small></p>
     </footer>
 </body>
 
