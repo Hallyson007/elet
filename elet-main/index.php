bbdb3328352007aec10f3c2599bf0b046a866bab
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2024-04-01 20:45:01 -0300

    Atualizado

---
+++ elet-main/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Loja de Eletrônicos</title>
+
+</head>
+
+<body>
+    
+    <header>
+        <div>
+            <!-- <button onclick="myFunction()">Dark Mode</button> -->
+        </div>
+        <h1>Loja de Eletrônicos</h1>
+        <nav>
+            <a href="#" id="tela">Início</a>
+            <a href="#" id="tela">Produtos</a>
+            <a href="#" id="tela">Contato</a>
+        </nav>
+    </header>
+    <main>
+        
+        <!-- Conteúdo principal aqui -->
+        <section id="idSection">
+            <h3>Produtos em Destaque</h2>
+            <span>40% OFF!</span>
+            <!-- Exemplo de lista de produtos -->
+            <ul type="circle">
+                <li>Fones de ouvidos</li>
+                <li>Luminárias</li>
+                <li>Capas p/ Diversos</li>
+            </ul>
+
+            <h3>Categorias</h3>
+            <ul type="circle">
+                <li><a href="#">Celulares</a></li>
+                <li><a href="#">Carregadores</a></li>
+                <li><a href="#">Games Retro 4k</a></li>
+                <li><a href="#">Caixas de som </a></li>
+                <li><a href="#">Fones de ouvidos</a></li>
+                <li><a href="#">Capas p/ Diversos</a></li>
+                <li><a href="#">Luminárias</a></li>
+                <li><a href="#">Mouse e Teclados</a></li>
+            </ul>
+        </section>
+        <div id="imgg">   <img src="img/1.jfif" alt="img">     </div>
+    </main>
+
+    <footer>
+        <p>© 2024 Loja de Eletrônicos</p>
+        <p>CIO hacker do bem.</p>
+        <p><small>Hallyson Developer</small></p>
+    </footer>
+</body>
+
+</html>
